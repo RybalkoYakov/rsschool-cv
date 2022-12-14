0f0ae304304c63dccf88b6ca763b2c74cd221138
RybalkoYakov <RybalkoYakovv@gmail.com>
2022-12-14 18:25:59 +0300

    refactor: theme color switch

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,13 @@ const header = document.querySelector('header')
 const navigationBurgerExpanded = document.querySelector('.navigation_burger__expanded')
 
 const STATE = {
-  burgerExpanded: false
+  burgerExpanded: false,
+  isDarkTheme: true
+}
+
+const THEME_COLORS_NAMES = {
+  bg: '--theme-bg-color-dark',
+  text: '--text-main-color-dark',
 }
 
 const DISPLAY = {
@@ -19,6 +25,10 @@ const SIZES = {
   hideNavigation: 1250,
 }
 
+window.onload = function (e) {
+  setTransitionToAllNodes(0.4)
+}
+
 window.addEventListener('resize', (e)=>{
   let width = e.target.innerWidth;
 
@@ -29,13 +39,17 @@ window.addEventListener('resize', (e)=>{
   }
 })
 
-
 window.addEventListener('pointerdown', (e) => {
   const targetClassList = e.target?.classList
 
   if (targetClassList?.contains('navigation_burger') || targetClassList?.contains('navigation_burger__line')) {
     toggleBurgerExpand();
   }
+
+  if (targetClassList.contains('input_switch__slider')) {
+    changeTheme()
+  }
+
 })
 
 function toggleNavigationMenu(hide_bool){
@@ -45,6 +59,7 @@ function toggleNavigationMenu(hide_bool){
   } else {
     navigationBurger.style.display = DISPLAY.none
     navigation.style.display = DISPLAY.block
+    navigationBurgerExpanded.style.display = DISPLAY.none
   }
 }
 
@@ -59,5 +74,31 @@ function toggleBurgerExpand(){
     header.style.alignItems = 'flex-start'
     STATE.burgerExpanded = true
     navigationBurgerExpanded.style.display = DISPLAY.block
+  }
+}
+
+function setTransitionToAllNodes(animationDuration = '0.4s', whichProperty = 'all', animationStyle = 'linear'){
+  const nodes = document.querySelectorAll('*')
+
+  nodes.forEach(value => {
+    value.style.transition = `${whichProperty} ${animationStyle} ${animationDuration}s`;
+  })
+}
+
+function changeTheme(){
+  const root = document.querySelector(':root')
+
+  if (STATE.isDarkTheme) {
+    root.style.setProperty(THEME_COLORS_NAMES.bg,'#ffffff')
+    root.style.setProperty(THEME_COLORS_NAMES.text,'#000000')
+    header.querySelector('.first').style.display = DISPLAY.block
+    header.querySelector('.second').style.display = DISPLAY.none
+    STATE.isDarkTheme = false
+  } else {
+    root.style.setProperty(THEME_COLORS_NAMES.bg,'#000000')
+    root.style.setProperty(THEME_COLORS_NAMES.text,'#ffffff')
+    header.querySelector('.first').style.display = DISPLAY.none
+    header.querySelector('.second').style.display = DISPLAY.block
+    STATE.isDarkTheme = true
   }
 }
